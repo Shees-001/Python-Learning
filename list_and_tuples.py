959241ed1e2b38bebec46a7b18cd0bea9bcc835a
Shees-001 <sheessillat@gmail.com>
2025-09-15 22:09:10 +0500

    List and tuples

---
+++ list_and_tuples.py
@@ -0,0 +1,69 @@
+# marks = [94.2, 95.2, 45.1, 62.8]
+# #marks.append(55.9)
+# # print(marks.sort(reverse=True))
+# # print(marks.reverse())
+# # marks.insert(1,77.0)
+# print(marks.remove(45.1))
+# print(marks)
+# print(marks[-3:-1])
+
+# print(type(marks))
+# print(len(marks))
+# print(marks[0])
+# print(marks[1])
+
+# student = ["Shees", 19, 99.3, "Karachi"]
+# print(student)
+
+# student[0] = "ABC"
+# print(student)
+
+
+
+#Tuples
+
+# tup = (87, 64, 33, 95, 76, 33)
+
+# print(tup.count(33))
+
+# Practice Questions
+
+#WAP to ask the user to enter names of their favorite movies and store then in a list
+
+# movies = []
+
+# movie1 = input("Enter your 1st favourite movies : ")
+# movie2 = input("Enter your 2nd favourite movies : ")
+# movie3 = input("Enter your 3rd favourite movies : ")
+
+# movies.append(movie1)
+# movies.append(movie2)
+# movies.append(movie3)
+
+# print(movies)
+
+# WAP to check if a list contain palindrome of elements
+
+#palindrome -> values are same in list when start or end  ex: ma'am , rececar 
+
+# list = [1, 2, 3, 2, 1]
+
+# copy = list.copy()
+
+# copy.reverse()
+
+# if(copy == list):
+#     print("palindrome")
+# else:
+#     print("Note palindrome")    
+
+
+# WAP to count the number of students with the grade "A" inthe following tuple
+
+tup = ("C", "D", "A", "A" , "B", "B", "A")
+print(tup.count("A"))
+
+list = ["C", "D", "A", "A" , "B", "B", "A"]
+
+print(list.sort())
+print(list)
